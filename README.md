60242b080896ccf0d023038894d71d1002a858c3
guillenjs <jorgeguilln@gmail.com>
2020-08-12 12:52:53 -0400

    Done.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # MONSTERS
 
-For this practice code challenge, your objective is to build a frontend for our
+<!-- For this practice code challenge, your objective is to build a frontend for our
 monsters data. Currently, the index.html page has a script tag to ```js/demo.js```
 so that you can see a demo of the finished app. Comment out the script tag to
 ```js/demo.js``` and uncomment out the script tag to ```js/index.js``` where you will
-code your solution
+code your solution -->
 
 ## Getting Started
 
-If you haven't yet, install json-server
+<!-- If you haven't yet, install json-server
 
 ```bash
 npm install -g json-server
@@ -18,22 +18,22 @@ Then run the server with
 
 ```bash
 json-server monsters.json
-```
+``` -->
 
 ## Objectives
 
-- When the page loads, show the first 50 monsters. Each monster's name, age, and
-  description should be shown.
-- Above your list of monsters, you should have a form to create a new monster.
+<!-- - When the page loads, show the first 50 monsters. Each monster's name, age, and
+  description should be shown. -->
+<!-- - Above your list of monsters, you should have a form to create a new monster.
   You should have fields for name, age, and description, and a 'Create Monster
   Button'. When you click the button, the monster should be added to the list
-  and saved in the API.
+  and saved in the API. -->
 - At the end of the list of monsters, show a button. When clicked, the button
   should load the next 50 monsters and show them.
 
 ## API methods
 
-Get the list of monsters
+<!-- Get the list of monsters -->
 
 ```text
 GET http://localhost:3000/monsters
---
+++ js/index.js
@@ -0,0 +1,107 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    createMonster();
+});
+
+// let page = 1
+
+fetch(`http://localhost:3000/monsters/?_limit=50&_page=1`) 
+.then(res => res.json())
+.then(monsterArr => monsterArr.forEach((monster) => {
+    renderMonster(monster)
+}))
+
+
+
+// let forwardButton = document.querySelector('#forward')
+//     forwardButton.addEventListener('click', (evt) => {
+//          page += 1
+//         console.log(page)
+//         // location.reload()
+//     })
+
+
+let allMonsters = document.querySelector('#monster-container')
+let formContainer = document.querySelector('#create-monster')
+
+
+let renderMonster = (monsterObj) => {
+    // create div container for each monster
+    let newDiv = document.createElement('div')
+
+    // Create h2 for name
+    let newH2 = document.createElement('h2')
+        newH2.innerText = monsterObj.name
+
+    // create h4 for age
+    let newH4 = document.createElement('h4')
+        newH4.innerText = monsterObj.age
+
+    // create paragraph for description example = Bio: monsterObj.descriptiopn
+    let newPar = document.createElement('p')
+        newPar.innerText = monsterObj.description
+
+    // append to monster div
+        newDiv.append(newH2, newH4, newPar)
+
+    // appened to allMonster container
+       allMonsters.append(newDiv)
+    formContainer.innerHTML = ""
+       
+    createMonster();
+}
+
+let createMonster = () => {
+    //new form to create monster with #monster-form
+    let newDiv = document.createElement('div')
+    let newH2 = document.createElement('h2')
+    let newH4 = document.createElement('h4')
+
+    let newForm = document.createElement('form')
+        newForm.id = "monster-form"
+
+    //create input for name with #name id and placeholder = 'name...'
+    let nameInput = document.createElement('input')
+        nameInput.id = 'name'
+        nameInput.placeholder = 'name...' 
+
+    //create input for age with #age id and placeholder = 'age..'
+    let ageInput = document.createElement('input')
+        ageInput.id = 'age'
+        ageInput.placeholder = 'age...'
+
+    //create input for description with #descriptipo id and placeholder = 'description..'
+    let desInput = document.createElement('input')
+        desInput.id = 'description'
+        desInput.placeholder = 'description...'
+
+    let createButton = document.createElement('button')
+        createButton.innerText = 'create'
+
+    newForm.append(nameInput, ageInput, desInput, createButton)
+    formContainer.append(newForm)
+
+    createButton.addEventListener("click", (evt) => {
+        evt.preventDefault()
+        console.log('I CLicked button')
+
+       
+        fetch('http://localhost:3000/monsters', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: nameInput.value,
+                age: ageInput.value,
+                description: desInput.value,
+            })
+        })
+        .then(res => res.json())
+        .then(newMonster => 
+            renderMonster(newMonster)
+
+        )
+    })
+}
+
+
